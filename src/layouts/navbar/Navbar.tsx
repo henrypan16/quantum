199ac095db2b847b43b0afb9e3a53d9415ded2b6
Henry Phan <hkphan@pm.me>
2024-06-03 17:19:57 -0400

    Implement local storage for credentials

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,13 +1,23 @@
-import { Left } from "./Left";
-import { Right } from "./Right";
+import {Notification} from './Notification'
+import {ToggleSearch} from './ToggleSearch'
+import {Application} from './Application'
+import {User} from './User'
+import {Search} from './Search'
+import {Button} from './Button'
+import {Logo} from './Logo'
 
 export const Navbar = () => {
   return (
     <>
         <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
             <div className="flex flex-wrap justify-between items-center">
-                <Left/>
-                <Right/>
+              <Button/>
+              <Logo/>
+              <Search/>
+              <ToggleSearch/>
+              <Notification/>
+              <Application/>
+              <User/>
             </div>
         </nav>
     </>
