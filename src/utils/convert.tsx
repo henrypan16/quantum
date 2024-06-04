9a77f0d1bc1b04f83908f04f3005538971f123c5
Henry Phan <hkphan@pm.me>
2024-06-04 17:19:49 -0400

    Add function to format data for display

--- src/utils/convert.tsx
+++ src/utils/convert.tsx
@@ -1,18 +1,51 @@
 import { TorrTorrentInfo } from '../utils/types';
+import  stateDictionary  from './StateDictionary';
 
 export const convertTorrentInfo = (info: TorrTorrentInfo) => {
     return {
         name: info.name,
-        size: info.size,
+        size: intToSize(info.size),
         progress: info.progress,
-        status: info.state,
+        status: stateDictionary[info.state].short,
         seeds: info.num_seeds,
         peers: info.num_leechs,
-        downSpeed: info.dlspeed,
-        upSpeed: info.upspeed,
+        downSpeed: intToSpeed(info.dlspeed),
+        upSpeed: intToSpeed(info.upspeed),
         ratio: info.ratio,
-        downloaded: info.downloaded,
-        uploaded: info.uploaded,
-        time: info.eta
+        downloaded: intToSize(info.downloaded),
+        uploaded: intToSize(info.uploaded),
+        time: secToTime(info.eta)
     }
+}
+
+
+
+export const intToSize = (size: number) => {
+    const units = ['B', 'KB', 'MB', 'GB', 'TB'];
+    let i = 0;
+    while (size > 1024 && i < units.length) {
+        size /= 1024;
+        i++;
+    }
+    return size.toFixed(2) + ' ' + units[i];
+}
+
+export const intToSpeed = (time: number) => {
+    const units = ['B/s', 'KB/s', 'MB/s', 'GB/s', 'TB/s'];
+    let i = 0;
+    while (time > 1024 && i < units.length) {
+        time /= 1024;
+        i++;
+    }
+    return time.toFixed(2) + ' ' + units[i];
+}
+
+export const secToTime = (time: number) => {
+    const units = ['s', 'm', 'h', 'd'];
+    let i = 0;
+    while (time > 60 && i < units.length) {
+        time /= 60;
+        i++;
+    }
+    return time.toFixed(2) + ' ' + units[i];
 }
--- src/utils/createETAString.ts
+++
@@ -1,7 +0,0 @@
-export const CreateETAString = (date: Date) => {
-  return [
-    `${date.getUTCHours() ? `${date.getUTCHours()}h` : ""}`,
-    `${date.getUTCMinutes() ? `${date.getUTCMinutes()}m` : ""}`,
-    `${date.getUTCSeconds() ? `${date.getUTCSeconds()}s` : ""}`,
-  ].join(" ");
-};
